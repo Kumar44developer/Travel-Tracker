05b3d0202244a0b55b64abcc95a20a2d3669f106
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-06 10:50:50 +0530

    Removed unnecessary whitespace in index.js

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import { pool } from "./db.mjs";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
